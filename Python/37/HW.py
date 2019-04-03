7d1ba09f3eec15dddf0d6c37ad8e92101d4c705f
Elisha Bernstein <bernsteinelisha@gmail.com>
2019-04-02 20:42:34 -0400

    class 38A

---
+++ Python/37/HW.py
@@ -0,0 +1,8 @@
+my_name = "Elisha Bernstein"
+my_address = "3 Kielt Way"
+people_I_know = ['Shloime Lubowsky', 'Zev Feldberger',
+                 'Getzel Waldman', 'Daniel Soloff']
+print(f'My Name Is: {my_name} My Address Is: {my_address}')
+print('These are the people I know from PCS {}'.format(people_I_know))
+print(my_name[::3])
+print(people_I_know[-2][1:-1])
