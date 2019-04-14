b0fb1faa33254c2532c3247bdd2f2b7d0e1bce7f
Elisha Bernstein <bernsteinelisha@gmail.com>
2019-04-13 23:34:39 -0400

    selection sorting

--- Python/40/HW 40.py
+++ Python/40/HW 40.py
@@ -1,4 +1,13 @@
-list1 = [65,33,17,87,54,25,78]
+#hard code a list like this
+#list1 = [65,33,17,87,54,25,78]
+import random
+#or use googles idea like this
+#list1 = (random.sample(range(0,100),10))
+#or use what we learned already like this
+list1 = [random.randint(0, 100) for y in range(1,11)]
+for i in range(len(list1)):
+    print(list1[i], end=" ")
+print("\n")   
 for i in range(len(list1)):
     lowest = i
     for j in range(i+1, len(list1)):
@@ -6,4 +15,4 @@ for i in range(len(list1)):
             lowest = j
     list1[i], list1[lowest] = list1[lowest], list1[i]  
 for i in range(len(list1)):
-    print(list1[i])        
+    print(list1[i], end=" ")        
