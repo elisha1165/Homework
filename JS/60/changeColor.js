3eaa9a7dc835b79db65e14e778a756fc6e3f16d9
Elisha Bernstein <bernsteinelisha@gmail.com>
2019-07-14 12:44:36 -0400

    60

--- JS/60/changeColor.js
+++ JS/60/changeColor.js
@@ -1,35 +1,72 @@
 (function () {
     'use strict';
-    const button1 = document.getElementById('button');
-    var myItnerval;
+    function get(id) {
+        return document.getElementById(id);
+    }
+    const button1 = get('button');
+    const table = get('myTable');
+    // originally I had it capture all the colors in an array, and when user hit stop it created a row with the time and the array
+    // i just couldnt figure hot how to loop through the array with innerHTML
+    //let colors = [];
+    let myItnerval;
+
+
     button1.addEventListener('click', function () {
-
-        if (document.getElementById('button').innerHTML === 'Start') {
-            document.getElementById('button').innerHTML = 'Stop';
-
-            myItnerval = setInterval(changeColor, 400);
-            console.log('changing');
+        if (get('button').innerHTML === 'Start') {
+            get('button').innerHTML = 'Stop';
+            myItnerval = setInterval(changeColor, 500);
+            const row = table.insertRow();
+            const time = row.insertCell();
+            const date = new Date();
+            time.innerHTML = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+            row.onclick = () => {
+                get('button').innerHTML = 'Stop';
+                const colorArr = []; 
+                let j = 0;
+                let t = 0;
+                    for (let i = 1; i < row.cells.length; i++) {
+                        colorArr[j++] = row.cells[i].innerHTML;
+                    }
+                    function theseColors(){
+                        if(t > row.cells.length){
+                            t = 0;
+                        }
+                        const color = colorArr[t++];
+                        document.body.style.backgroundColor = color; 
+                    }
+                    myItnerval = setInterval(theseColors, 500);
+            };
+    /*row.addEventListener('click', function () {
+        get('button').innerHTML = 'Stop';
+        while (get('button').innerHTML === 'Stop') {
+            for (var i = 1; i < row.cells.length; i++) {
+                document.body.style.backgroundColor = row.cells[i].innerHTML;
+            }
         }
+    });*/
+}
         else {
-            clearInterval(myItnerval);
-            document.getElementById('button').innerHTML = 'Start';
-
-        }
+    clearInterval(myItnerval);
+    get('button').innerHTML = 'Start';
+}
     });
 
-    function getColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        console.log(color);
-        return color;
+function getColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
+    return color;
+}
 
-    function changeColor() {
-        document.body.style.backgroundColor = getColor();
-    }
-
+function changeColor() {
+    const color = getColor();
+    document.body.style.backgroundColor = color;
+    //colors.push(document.body.style.backgroundColor);
+    const lastRow = table.rows[table.rows.length - 1];
+    const addColors = lastRow.insertCell();
+    addColors.innerHTML = color;
+}
 
 }());
