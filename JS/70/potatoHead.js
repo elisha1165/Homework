023703bc8f735421fb3d9f3b225d75d3d230b53c
Your Name <bernsteinelisha@gmail.com>
2019-09-17 21:18:18 -0400

    hw71 & unfinished potato

--- JS/70/potatoHead.js
+++ JS/70/potatoHead.js
@@ -8,10 +8,16 @@
     const postP = $("#postP");
     const dragP = $("#dragP");
 
+    // let dragging;
+    // let offset;
+
+
+
     //Will show ONLY the body parts of the selected correspanding button
     $('.piecebtn').each(function () {
         $(this).click(function () {
             $('.piece').hide();
+            $(".moved").show();
             $(`.${$(this).text()}`).show();
         });
     });
@@ -22,6 +28,7 @@
         postBtn.css("color", "black");
         dragP.show();
         postP.hide();
+
     });
 
     //will make body parts positionable & show correct text, and turn button blue
@@ -32,5 +39,62 @@
         dragP.hide();
     });
 
-    
+
+    // $(document)
+    //     .on('mousedown', '.piece', e => {
+    //         console.log('down', e);
+    //         dragging = $(e.target);
+    //         offset = { y: e.offsetY, x: e.offsetX };
+    //     }).mousemove(e => {
+    //         if (dragging) {
+    //             console.log('move', e);
+    //             dragging.css({ top: e.pageY - offset.y, left: e.pageX - offset.x });
+    //             e.preventDefault();
+    //         }
+    //     }).mouseup(e => {
+    //         console.log('up', e);
+    //         dragging = null;
+    //     });
+
+    // const keep = function (e, ui) {
+    //     console.log("moved");
+    //     ui.addClass("moved");
+    // };
+
+    $(".piece").draggable({
+        stack: ".piece",
+        //stop: keep( ui)//,
+        stop: function () { $(this).addClass('moved'); }
+        //containment: "parent"
+    });
+
+    function saveState() {
+        const state = [];
+        $(".piece").each((index, elem) => {
+            const part = $(elem);
+            state.push({
+                part: part.attr('src'),
+                top: part.css('top'),
+                left: part.css('left'),
+                zIndex: part.css('z-index')
+            });
+        });
+        localStorage.parts = JSON.stringify(state);
+    }
+
+    $("#click").click(() => {
+        saveState();
+    });
+
+    if (localStorage.parts) {
+        const parts = JSON.parse(localStorage.parts);
+        parts.forEach(part => {
+            $(`img[src="${part.part}"]`).css({ top: part.top, left: part.left, zIndex: part.zIndex });
+            //part.addClass('moved');
+            //part.classList.add("moved");
+            console.log(part);
+            //maxZindex = Math.max(part.zIndex, maxZindex);
+        });
+        $(".moved").show();
+    }
 }());
---
+++ JS/71/vehicles.js
@@ -0,0 +1,38 @@
+(function () {
+    'use strict';
+
+    function Vehicle(color/*, speed = 0*/) {
+        this.color = color;
+    }
+
+    Vehicle.prototype.go = function (speed) {
+        this.speed = speed;
+        console.log(`Vehicle is now going ${this.speed} miles an hour`);
+    };
+
+    Vehicle.prototype.print = function () {
+        this.speed ? console.log(`This vehicle is ${this.color} and going ${this.speed} miles an hour`) : console.log(`This vehicle is ${this.color}`);
+    };
+
+    const greenCar = new Vehicle('green');
+
+    greenCar.print();
+    greenCar.go(50);
+    greenCar.print();
+
+    function Plane(color) {
+        Vehicle.call(this, color);
+    }
+    Plane.prototype = Object.create(Vehicle.prototype);
+
+    // Plane.prototype.print = function () {
+    //     console.log(`This plane is ${this.color} and f;ying ${this.speed} miles an hour`);
+    // };
+    Plane.prototype.go = function (speed) {
+        this.speed = speed;
+        console.log(`Plane is now flying ${this.speed} miles an hour`);
+    };
+    const bluePlane = new Plane('blue');
+    bluePlane.go(735);
+    bluePlane.print();
+}());
