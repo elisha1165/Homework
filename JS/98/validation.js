2c1e59adb84b111be928a98bfe3cf6e301c4c750
Elisha Bernstein <you@example.com>
2020-01-28 20:33:56 -0500

    EC changes the error, not sure why

---
+++ JS/98/validation.js
@@ -0,0 +1,16 @@
+module.exports = function (req, res, next) {
+    if (req.query.magicword) {
+        if (req.query.magicword != "Please") {
+            throw new Error('Please enter the correct magic word and try again');
+        } else if (req.query.magicword === "Please") {
+            next();
+        }
+    } else {
+        throw new Error('Please enter the magic word before trying to login');
+    }
+
+    app.use((error, req, res, next) => {
+        res.statusCode = 401;
+        res.end(error.message);
+    });
+};
